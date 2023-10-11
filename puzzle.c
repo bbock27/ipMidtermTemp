052d0857e4005317a8081d3c474be77fc38e9922
bbock27 <bradybock11@icloud.com>
2023-10-10 22:57:37 -0400

    added command loop for stdin input

--- puzzle.c
+++ puzzle.c
@@ -4,11 +4,42 @@
 #include "ppm_io.h"
 #include "puzzle.h"
 
-// int main(int argc, char **argv) {
-//   if (argc < 1 || argc > 2) {
-//     fprintf(stderr, "Usage: ./puzzle [<command file>]\n");
-//     return 1;
-//   }
+int main(int argc, char **argv) {
+  if (argc < 1 || argc > 2) {
+    fprintf(stderr, "Usage: ./puzzle [<command file>]\n");
+    return 1;
+  }
 
-  // TODO: implement
-// }
+    char command = '\0';
+    Puzzle *puzzle = NULL;
+  while(scanf("%c", &command) != 0){
+    if(command == 'c'){
+        handleCcommand();
+    }
+    else if(command == 't'){
+        handleTcommand();
+    }
+    else if(command == 'i'){
+        handleIcommand();
+    }
+    else if(command == 'p'){
+        handlePcommand(puzzle);
+    }
+    else if(command == 'w'){
+        handleWcommand();
+    }
+    else if(command == 's'){
+        handleScommand(puzzle);
+    }
+    else if(command == 'k'){
+        handleKcommand(puzzle);
+    }
+    else if(command == 'v'){
+        handleVcommand(puzzle);
+    }
+    else if(command == 'q'){
+        handleQcommand();
+    }
+  }
+
+}
--- puzzle.h
+++ puzzle.h
@@ -16,6 +16,19 @@ typedef struct {
 // TODO: function declarations
 
 
+//TODO
+void handleIcommand();
+void handleQcommand();
+void handleTcommand();
+void handleWcommand();
+
+int handleKcommand(Puzzle *puzzle);
+void handleVcommand(Puzzle *puzzle);
+void handlePcommand(Puzzle *puzzle);
+void handleScommand(Puzzle *puzzle);
+Puzzle *handleCcommand();
+
+
 Puzzle *createPuzzle(int size);
 Puzzle *copyPuzzle(int *grid, int size, char *image);
 int solve(Puzzle *puzzle, int maxMoves);
@@ -25,16 +38,12 @@ int solve(Puzzle *puzzle, int maxMoves);
 int findEmptyIndex(Puzzle *puzzle);
 
 
-void handleVcommand(Puzzle *puzzle);
-Puzzle *handleCcommand();
-void handlePcommand(Puzzle *puzzle);
-void handleScommand(Puzzle *puzzle);
-
-
 void setPuzzleTile(Puzzle *puzzle, int col, int row, int newVal);
 int getPuzzleTile(Puzzle *puzzle, int col, int row);
 void setPuzzleImage(Puzzle *, char *imageFile);
 void setPuzzleGrid(Puzzle *, int *);
+
+
 void changePuzzleImage(Puzzle *, char *imageFile);
 void changePuzzleGrid(Puzzle *, int *);
 void deletePuzzle(Puzzle *puzzle);
